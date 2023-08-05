9b16ace7e1e41b38a44916f2cfba42195e84f231
assim fayas <asim1fayas@gmail.com>
2023-08-05 21:48:38 +0530

    login ui completed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './component/users/home/home.component';
+import { LoginComponent } from './component/users/login/login.component';
+import { RegisterComponent } from './component/users/register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", component: HomeComponent },
+  { path: "register", component: RegisterComponent },
+  { path: "login", component: LoginComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

1fad69564301cf851ccbc7c82fb72c53b0c7b3f9
assim fayas <asim1fayas@gmail.com>
2023-08-05 11:03:54 +0530

    initial setup

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { RegisterComponent } from './component/users/register/register.component';
+import { LoginComponent } from './component/users/login/login.component';
+import { HomeComponent } from './component/users/home/home.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    RegisterComponent,
+    LoginComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }

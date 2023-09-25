890176c3c8078f508156d0728f32ad31d2a9794a
assim fayas <asim1fayas@gmail.com>
2023-09-25 22:47:16 +0530

    appoinment adding in admin side styled

---
+++ src/app/component/users/slots/slots.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-slots',
+  templateUrl: './slots.component.html',
+  styleUrls: ['./slots.component.css']
+})
+export class SlotsComponent {
+
+}

8ba8065ce4013072fff95fe448e13b5a72bf3552
assim fayas <asim1fayas@gmail.com>
2023-10-19 14:27:29 +0530

    routing isuue solved

--- src/app/socketio/socket-io.config.ts
+++ src/app/socketio/socket-io.config.ts
@@ -1,6 +1,6 @@
 import { SocketIoConfig } from "ngx-socket-io";
 const config: SocketIoConfig = {
-    url: 'http://localhost:3000', options: {}
+    url: 'http://loclahost:3000:', options: {}
 };
 
 export { config };

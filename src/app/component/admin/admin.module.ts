cc354147c74aba691f26b3843bf376117f96d379
assim fayas <asim1fayas@gmail.com>
2023-12-11 11:52:57 +0530

    code cleaned

--- src/app/component/admin/admin.module.ts
+++ src/app/component/admin/admin.module.ts
@@ -10,47 +10,14 @@ import { NavsidebarComponent } from './navsidebar/navsidebar.component';
 import { FlagComponent } from './flag/flag.component';
 import { CardModule } from 'primeng/card';
 import { TagModule } from 'primeng/tag';
-
-
 import { TooltipModule } from 'primeng/tooltip';
 import { BarchartComponent } from 'src/app/barchart/barchart.component';
 import { PiechartComponent } from 'src/app/piechart/piechart.component';
 import { ProfileComponent } from './profile/profile.component';
 import { NgxChartsModule } from '@swimlane/ngx-charts';
-import { APP_INITIALIZER } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { environment } from 'src/environments/environment';
-import { DataService } from 'src/app/service/data.service';
 import { ChartModule } from 'primeng/chart';
 import { ViewExpertComponent } from './view-expert/view-expert.component';
 import { SharedModule } from 'src/app/shared/shared.module';
-
-
-// import {MatTableModule} from '@angular/material/table'
-// import {MatPaginatorModule}from '@angular/material/paginator'
-// import {MatSortModule}from '@angular/material/sort'
-// import {MatFormFieldModule}from '@angular/material/form-field'
-// import{MatInputModule}from '@angular/material/input'
-
-
-// const URL = environment.adminUrl
-
-// Function to load the data from the backend
-// export function loadData(dataService: DataService, httpClient: HttpClient) {
-//   return () => {
-//     httpClient.get(`${URL}/adminPie`).subscribe((data: any) => {
-//       dataService.data = data;
-//     });
-//   };
-// }
-
-
-
-
-
-
-
-
 
 
 @NgModule({
@@ -63,13 +30,6 @@ import { SharedModule } from 'src/app/shared/shared.module';
     FlagComponent,
     ProfileComponent,
     ViewExpertComponent,
-
-
-
-
-
-
-
   ],
   imports: [
     CommonModule,
@@ -84,22 +44,8 @@ import { SharedModule } from 'src/app/shared/shared.module';
     NgxChartsModule,
     ChartModule,
     SharedModule
-
-
-
-    // MatTableModule,
-    // MatPaginatorModule,
-    // MatSortModule,
-    // MatFormFieldModule,
-    // MatInputModule,
-
-
-
-
   ],
 
-  providers: [
-
-  ],
+  providers: [],
 })
 export class AdminModule { }

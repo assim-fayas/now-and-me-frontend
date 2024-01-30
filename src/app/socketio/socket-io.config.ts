5292c62957fcb6a9585e99677d14e7c34c7b1fd2
assim fayas <asim1fayas@gmail.com>
2024-01-30 18:12:31 +0530

    domain updated

--- src/app/socketio/socket-io.config.ts
+++ src/app/socketio/socket-io.config.ts
@@ -1,6 +1,6 @@
 import { SocketIoConfig } from "ngx-socket-io";
 const config: SocketIoConfig = {
-    url: 'https://joymart.store', options: {}
+    url: 'https://nowandmee.joymart.store', options: {}
 };
 
 export { config };
